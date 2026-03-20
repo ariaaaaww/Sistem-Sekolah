e08139634b03c8164d7e7fdae3a5ca45526264c7
ariaaaawww <ariantowp0@gmail.com>
2026-03-20 16:05:08 +0700

    gabut

--- app/view/karya/index.php
+++ app/view/karya/index.php
@@ -1,12 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .gas a {
+            text-decoration: none;  
+            font-size: 200px;
+            display: flex;
+            justify-content: center;
+            color: blanchedalmond;
+        }
+
+        .gas :hover {
+            color: cyan;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>yayayayayayayaya</h1>
-    <h1>yayayayayayayaya</h1>
+    <div class="gas">
+        <a href="/deks/deksripsi-karya/" class="btn-detail">
+            Deksripsi
+        </a>
+    </div>
+
 </body>
+
 </html>
--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ use App\Core\Router;
 $router = new Router();
 
 //Karya
-$router->add('GET', '/deks', 'KaryaController', 'index');
-$router->add('GET', '/deks/deksripsi-karya', 'KaryaController', 'deskripsiKarya');
+$router->add('GET', '/', 'KaryaController', 'index');
+$router->add('GET', '/deks/deksripsi-karya/', 'KaryaController', 'deskripsiKarya');
 // $router->add('GET', '/deks/lampiran-karya', 'KaryaController', 'lampiranKarya');
-$router->add('GET', '/deks/lampiran-karya/{id}', 'KaryaController', 'lampiranKarya');
+$router->add('GET', '/deks/lampiran-karya/{id}/', 'KaryaController', 'lampiranKarya');
 
 $router->run();
 
