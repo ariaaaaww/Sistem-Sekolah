61c7d0fce2dd952b43fd00ef6c5015332efeeb07
ariaaaawww <ariantowp0@gmail.com>
2026-04-08 09:11:01 +0700

    feat: improvement to view mechanism

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,28 +1,39 @@
 <?php
 namespace App\Controllers;
+require_once '../app/core/Controller.php';
 
-class StudentController
+use App\Core\Controller;
+
+class StudentController extends Controller
 {
     public function index()
     {
-        require_once '../app/view/students/index.php';
+        // require_once '../app/view/students/index.php';
+        $this->view('students.index');
     }
     public function create()
     {
-        require_once '../app/view/students/create.php';
+        // require_once '../app/view/students/create.php';
+        $this->view('students.create');
     }
 
     public function show(string $id)
     {
-        require_once "../app/view/students/show.php";
+        // require_once "../app/view/students/show.php";
+        $this->view('students.show');
     }
 
-    public function edit(string $id)
+    public function edit()
     {
-        require_once '../app/view/students/edit.php';
+        // require_once '../app/view/students/edit.php';
+        $this->view('students.edit');
     }
+
+
 }
+
+
 ?>
 
 <!-- ../ untuk dari awal -->
-<!-- .. untuk dari folder dari file -->
+<!-- ./ untuk dari folder dari file -->
---
+++ app/core/Controller.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Core;
+
+class Controller
+{
+    public function view(string $view, array $data = [])
+    {
+        extract($data);
+        $view = str_replace('.', '/', $view);
+        require_once "../app/view/{$view}.php";
+    }
+}
+?>
